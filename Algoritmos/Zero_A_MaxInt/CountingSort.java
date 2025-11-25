0b75ca694d12c5710c486438fd890efd63576076
Moises Lutz <moiseshenriquelutz2006@gmail.com>
2025-11-24 22:55:20 -0300

    mudança

--- Algoritmos/Zero_A_MaxInt/CountingSort.java
+++ Algoritmos/Zero_A_MaxInt/CountingSort.java
@@ -32,34 +32,34 @@ public class CountingSort {
     }
 
        public static void dezMil() {
-        int[] vetor = IO.gerarValoresAleatorios(Integer.MAX_VALUE-1, 0, Integer.MAX_VALUE-1);
+        int[] vetor = IO.gerarValoresAleatorios(10000, 0, Integer.MAX_VALUE-1);
         long tempo_inicio = IO.medirTempoIniciar();
         IO.countingSort(vetor);
         double tempoTotal = IO.medirTempoFinalizarSegundos(tempo_inicio);
-        IO.imprimirTempo(Integer.MAX_VALUE-1, tempoTotal);
+        IO.imprimirTempo(10000, tempoTotal);
     }
 
        public static void cemMil() {
-        int[] vetor = IO.gerarValoresAleatorios(Integer.MAX_VALUE-10, 0, Integer.MAX_VALUE-1);
+        int[] vetor = IO.gerarValoresAleatorios(100000, 0, Integer.MAX_VALUE-1);
         long tempo_inicio = IO.medirTempoIniciar();
         IO.countingSort(vetor);
         double tempoTotal = IO.medirTempoFinalizarSegundos(tempo_inicio);
-        IO.imprimirTempo(Integer.MAX_VALUE-10, tempoTotal);
+        IO.imprimirTempo(100000, tempoTotal);
     }
 
        public static void umMilhao() {
-        int[] vetor = IO.gerarValoresAleatorios(Integer.MAX_VALUE-100, 0, Integer.MAX_VALUE-1);
+        int[] vetor = IO.gerarValoresAleatorios(1000000, 0, Integer.MAX_VALUE-1);
         long tempo_inicio = IO.medirTempoIniciar();
         IO.countingSort(vetor);
         double tempoTotal = IO.medirTempoFinalizarSegundos(tempo_inicio);
-        IO.imprimirTempo(Integer.MAX_VALUE-100, tempoTotal);
+        IO.imprimirTempo(1000000, tempoTotal);
     }
 
        public static void dezMilhoes() {
-        int[] vetor = IO.gerarValoresAleatorios(Integer.MAX_VALUE-1000, 0, Integer.MAX_VALUE-1);
+        int[] vetor = IO.gerarValoresAleatorios(10000000, 0, Integer.MAX_VALUE-1);
         long tempo_inicio = IO.medirTempoIniciar();
         IO.countingSort(vetor);
         double tempoTotal = IO.medirTempoFinalizarSegundos(tempo_inicio);
-        IO.imprimirTempo(Integer.MAX_VALUE-1000, tempoTotal);
+        IO.imprimirTempo(10000000, tempoTotal);
     }
 }
